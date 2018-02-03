71681fdd27ef653c0e6bc5f1985c755143a1fa3e
Oghenenyerhov Akushe <ero@Oghenenyerhovs-MacBook.local>
2018-02-02 23:09:19 -0600

    First commit

---
+++ Swift Language Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Language Basics
+//
+//  Created by Oghenenyerhov Akushe on 2/2/18.
+//  Copyright © 2018 Oghenenyerhov Akushe. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
